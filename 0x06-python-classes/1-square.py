670c5ee7c20f629d669d8b7baae2cef982b81e8b
AlexVoid14 <fortuinalexander48@gmail.com>
2023-03-28 13:44:18 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""Define a class Square."""
-
-class Square:
-    """Represent a square."""
+# 0-square.py by Alexander Fortuin
+"""Square module."""
 
 def __init__(self, size):
-    """Initialize a new Square.-
-
-Args:
-size (int): The size of the new square.
-"""
-self.__size = size
+    """Constructor.
+    Args:
+    size: length of side of the square.
+    """
+    self.__size = size
