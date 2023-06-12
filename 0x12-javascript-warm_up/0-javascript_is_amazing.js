62a14af1391430bf74f30eb43c91c701d7756476
Alexander Fortuin <fortuinalexander48@gmail.com>
2023-06-12 13:31:59 +0000

    mandatory

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 
-const myVar = "JavaScript is amazing";
+#!/usr/bin/node
+
+// Script that prints "JavaScript is amazing".
+
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,2 @@
+t count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
